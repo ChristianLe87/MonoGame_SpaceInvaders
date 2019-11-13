593bcdec785da4db42c5a92b9acac089372a78f0
Christian <christianlehnhoff@gmail.com>
2019-11-13 21:18:09 +0100

    Add button

---
+++ Shared/Assets/Button.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Shared
+{
+    public class Button
+    {
+        Texture2D noState_Texture;
+        Texture2D hoverState_Texture;
+        Texture2D pressedState_Texture;
+
+        Rectangle rectangle;
+
+        string buttonState = "";
+
+        public Button(Rectangle rectangle)
+        {
+            this.noState_Texture = Tools.CreateColorTexture(Color.Gray);
+            this.hoverState_Texture = Tools.CreateColorTexture(Color.LightGray);
+            this.pressedState_Texture = Tools.CreateColorTexture(Color.DarkGray);
+
+            this.rectangle = rectangle;
+
+            this.buttonState = "noState";
+        }
+
+        public void Update()
+        {
+            MouseState mouseState = Mouse.GetState();
+
+
+            if (rectangle.Contains(mouseState.X, mouseState.Y))
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed)
+                    this.buttonState = "pressedState";
+                else
+                    this.buttonState = "hoverState";
+            }
+            else
+            {
+                this.buttonState = "noState";
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            switch (buttonState)
+            {
+                case "noState":
+                    spriteBatch.Draw(noState_Texture, rectangle, Color.White);
+                    break;
+                case "hoverState":
+                    spriteBatch.Draw(hoverState_Texture, rectangle, Color.White);
+                    break;
+                case "pressedState":
+                    spriteBatch.Draw(pressedState_Texture, rectangle, Color.White);
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+    }
+}
--- Shared/Scenes/Menu.cs
+++ Shared/Scenes/Menu.cs
@@ -6,18 +6,22 @@ namespace Shared
 {
     public class Menu : ILevel
     {
+        Button button;
 
         public Menu(ContentManager content)
         {
+            button = new Button(new Microsoft.Xna.Framework.Rectangle(100,100,100,100));
         }
 
         public void Update()
         {
+            button.Update();
             //throw new NotImplementedException();
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
+            button.Draw(spriteBatch);
             //throw new NotImplementedException();
         }
     }
